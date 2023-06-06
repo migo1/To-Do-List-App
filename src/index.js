93a83aa91ab5daffdb81b6e0157e2bde433320be
migo1 <ertmigot@gmail.com>
2023-06-06 13:54:20 +0300

    setup-linters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import _ from "lodash";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
